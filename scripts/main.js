f3abb2cc4c465f0689b36919d32686d62fef014f
Kevin Cocquyt <kevin_cocquyt@outlook.com>
2021-11-22 20:21:28 +0100

    json data

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
 console.log("load");
 
-$(".nav-link").on("click", function(){
-	$(".nav-link").toggleClass("active", false);
-	$(this).toggleClass("active", true);
+$(".nav-link").on("click", function () {
+    $(".nav-link").toggleClass("active", false);
+    $(this).toggleClass("active", true);
 });
+
+var app = Vue.createApp({
+    data() {
+        return {
+            youthList: [],
+        };
+    },
+    created() {
+        const self = this;
+
+        $.getJSON("/data/youth.json", function (data) {
+            for (item of data) {
+                self.youthList.push({
+                    group: item.group,
+                    born: item.born,
+                });
+            }
+        });
+    },
+});
+
+app.mount("#main");
